b683b8db1727e88ec540d9c80014672ec2f8ed59
Insonus.K <insonus.K@gmail.com>
2019-08-24 01:44:07 +0300

    Add TextSplit.Tool.ByLineLenght

---
+++ TextSplit/TextSplit.Tool.ByLineLenght/TextLineSplitterExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace TextSplit.Tool.ByLineLenght
+{
+    public static class TextLineSplitterExtension
+    {
+        public static string[] SplitByLineLenght(this string text, int length)
+        {
+            LineSplitter _splitter = new LineSplitter(length);
+            return _splitter.Split(text);
+        }
+    }
+}
